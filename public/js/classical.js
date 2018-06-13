f582c5328bedf6fa32717a9d4c6e26090b5283c0
Matt Sherman <mwsherman@gmail.com>
2018-06-12 23:52:25 -0400

    single player

--- public/js/classical.js
+++ public/js/classical.js
@@ -1,24 +1,46 @@
-$(function() {
-	$('audio').mediaelementplayer({
-		mode: 'auto',
-		audioWidth: 120,
-		audioHeight: 120,
-		features: ['playpause'],
-		pluginPath: '/mediaelement/',
-		success : function(me, node) {
-			var status = $("#status small");
+$(function(){
 
-			me.addEventListener('play', function() {
-				status.text('Loading...');
-			}, false);
-
-			me.addEventListener('playing', function() {
-				status.text('Playing');
-			}, false);
-
-			me.addEventListener('pause', function() {
-				status.text('Paused');
-			}, false);
-		}
+	var audio = document.getElementById('player');
+	var status;
+	var player = new MediaElementPlayer(audio, {
+	  mode: 'auto',
+	  audioWidth: 120,
+	  audioHeight: 120,
+	  features: ['playpause'],
+	  pluginPath: '/js/mediaelement/',
+	  success: function(me, node) {
+		me.addEventListener(
+		  'play',
+		  function() {
+			status.text('Loading...');
+		  },
+		  false
+		);
+	
+		me.addEventListener(
+		  'playing',
+		  function() {
+			status.text('Playing');
+		  },
+		  false
+		);
+	
+		me.addEventListener(
+		  'pause',
+		  function() {
+			status.text('Paused');
+		  },
+		  false
+		);
+	  }
 	});
+	
+	$(document).on('click', '.station', function(e) {
+		player.pause();
+		$('.status small').text('Click to play');
+		status = $(this).find('.status small');
+		player.setSrc($(this).data('src'));
+		player.play();
+		e.preventDefault();
+	});	
 });
