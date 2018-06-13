631c24d90a0c3d9343a326a5c5aa3ed00bf7cbbf
Matt Sherman <mwsherman@gmail.com>
2018-06-13 15:32:28 -0400

    rewrite classical to use native bits

---
+++ content/classical/_index.md
@@ -0,0 +1,8 @@
+---
+title: "A simple classical radio player"
+css:
+- "/css/classical.css"
+js:
+- "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+- "/js/classical.js"
+---
--- static/js/classical.js
+++ static/js/classical.js
@@ -1,46 +1,43 @@
 $(function(){
 
-	var audio = document.getElementById('player');
-	var status;
-	var player = new MediaElementPlayer(audio, {
-	  mode: 'auto',
-	  audioWidth: 120,
-	  audioHeight: 120,
-	  features: ['playpause'],
-	  pluginPath: '/js/mediaelement/',
-	  success: function(me, node) {
-		me.addEventListener(
-		  'play',
-		  function() {
-			status.text('Loading...');
-		  },
-		  false
-		);
-	
-		me.addEventListener(
-		  'playing',
-		  function() {
-			status.text('Playing');
-		  },
-		  false
-		);
-	
-		me.addEventListener(
-		  'pause',
-		  function() {
-			status.text('Paused');
-		  },
-		  false
-		);
-	  }
-	});
-	
+	var control = document.getElementById('wrapper');
+	control.addEventListener('click', function() { 
+		audio.paused ? audio.play() : audio.pause();
+	}, true);
+
+	var audio = document.getElementById('audio');
+	audio.addEventListener('playing', function() { 
+		control.classList.add('playing');
+		control.classList.remove('waiting');
+	}, true);
+	audio.addEventListener('pause', function() {
+		control.classList.remove('playing');
+	}, true);
+	audio.addEventListener('waiting', function() {
+		control.classList.add('waiting');
+	}, true);
+
+	var current;
+
 	$(document).on('click', '.station', function(e) {
-		player.pause();
-		$('.status small').text('Click to play');
-		status = $(this).find('.status small');
-		player.setSrc($(this).data('src'));
-		player.play();
+		var station = $(this);
+
+		if (current && station.attr('id') === current.attr('id')) {
+			return;
+		}
+
+		current = station;
+		audio.pause();
+		audio.src = station.data('src');
+
+		var c = $(control);
+
+		c.slideUp(200, function(){
+			audio.play();
+			station.after(control);
+			c.slideDown();
+		});
+
 		e.preventDefault();
 	});	
 });
