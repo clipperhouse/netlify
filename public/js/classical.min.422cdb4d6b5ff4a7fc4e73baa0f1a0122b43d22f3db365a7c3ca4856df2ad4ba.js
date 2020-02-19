7e1084b2dd970e14f800865e1b33302105e03834
Matt Sherman <mwsherman@gmail.com>
2020-02-19 16:35:44 -0500

    js at the bottom instead

---
+++ public/js/classical.min.422cdb4d6b5ff4a7fc4e73baa0f1a0122b43d22f3db365a7c3ca4856df2ad4ba.js
@@ -0,0 +1,7 @@
+"use strict";(function(){const faq=document.getElementById('faq');const what=document.getElementById('what');if(faq&&what){faq.classList.add('hide');what.addEventListener('click',e=>{e.stopPropagation();faq.classList.toggle('hide');});what.classList.add('clickable');}
+const control=document.getElementById('wrapper');if(!control){return;}
+const audio=document.getElementById('audio');if(!audio){return;}
+control.addEventListener('click',()=>{audio.paused?audio.play():audio.pause();});audio.addEventListener('playing',()=>{control.classList.add('playing');control.classList.remove('waiting');});audio.addEventListener('pause',()=>{control.classList.remove('playing');});audio.addEventListener('waiting',()=>{control.classList.add('waiting');});let stations=Array();for(const el of document.getElementsByClassName('station')){const src=el.getAttribute('data-src');if(!src){console.log('station '+el.id+' lacks a data-src attribute');continue;}
+const station={el,src};stations.push(station);}
+const $c=$(control);let current;for(const station of stations){station.el.addEventListener('click',e=>{var _a;e.stopPropagation();if(((_a=current)===null||_a===void 0?void 0:_a.el.id)===station.el.id){return;}
+current=station;audio.pause();audio.src=current.src;$c.slideUp(250,()=>{audio.play();station.el.after(control);$c.slideDown();});});}})();
